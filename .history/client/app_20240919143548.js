d3bf8da4cff83325a97b1aa40f49b2098973a5a6
Anthony Ricketts <mra.ricketts@gmail.com>
2024-09-20 06:29:28 +0100

    first commen

---
+++ .history/client/app_20240919143548.js
@@ -0,0 +1,25 @@
+// Dom manipulation
+//select the form
+// select feedback container
+
+//FORM
+//we need an event to submit the form data
+
+// the event handler
+//prevent default behavior
+//a formData object template
+//get the formValues to insert them into the FormDATA object
+
+//
+//fetch the CREATE endpoint
+// fetch('localhost-url'/endpoint'),
+//{
+//   method:
+// headers:
+// body:
+// }
+
+// the event listener  ----> submit
+
+// FEEDBACK container
+//fetch read the end
---
+++ server/.history/server_20240919140537.js
@@ -0,0 +1,17 @@
+//access express,cors,pg,dotenv
+// initialise express
+
+//configure dot env
+
+//i need to tell my express pp to use json
+//i need to tell my express pp to use cors
+
+// i need to set up port  for my app to listen
+
+// need to set up root route
+
+//you need teo routes
+// read the database data
+// create or add new data to database
+//!in your create route, the reuest.body is an object theat represents the form data coming from your client
+// you need to use sql queries and parameters in these routes
